5604299cada23a2125066915d2fa3175edf4a7a6
Julio Mendes <juliomendes160@hotmail.com>
2024-12-04 13:35:58 -0300

    2.4 Estrutura condicional : if-elseif-else

--- Estruturas Condicionais e de Repetição em PHP/condicionalIfElse.php
+++ Estruturas Condicionais e de Repetição em PHP/condicionalIfElse.php
@@ -1,82 +1,13 @@
 <?php
 
-$nota = 7;
+$nota = 1;
 
-// if ($nota >=7 ) {
-//   echo "Aluno(a) aprovado !";
-// }else {
-//   echo "Aluno(a) reprovado !";
-// }
+if ($nota >=7 ) {
+  echo "Aluno(a) aprovado!";
+} else if (($nota < 7) && ($nota >=4)) {
+    echo "Aluno(a) está em recuperação!";
+} else if (($nota < 4) && ($nota >=2)) {
+    echo "Aluno(a) está em conselho de classe!";
+}
 
- echo $nota >=7 ?  "Aluno(a) aprovado !" :  "Aluno(a) reprovado !";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if ($variavel1 or $variavel2) {
-
-//   //Resultado é verdadeiro pois uma das variáveis é verdadeira
-//   echo "Verdadeiro";
-
-// } else {
-
-//   echo "Falso";
-
-// }
-
-// if ($variavel1 xor $variavel3) {
-
-//   echo "Verdadeiro";
-
-// } else {
-
-//   //Resultado é falso pois os valores de ambas as variáveis são verdadeiras
-//   echo "Falso";
-
-// }
-
-// if (!$variavel2) {
-
-//   //Resultado é verdadeiro pois o valor da variável é falso
-//   echo "Verdadeiro";
-
-// } else {
-
-//   echo "Falso";
-
-// }
-
-// if ($variavel1 && $variavel3) {
-
-//   //Resultado é verdadeiro pois ambos os valores das variáveis são verdadeiros
-//   echo "Verdadeiro";
-
-// } else {
-
-//   echo "Falso";
-
-// }
-
-// if (!$variavel1 || !$variavel3) {
-
-//   echo "Verdadeiro";
-
-// } else {
-
-//   //Resultado é falso, pois o valor de ambas as variáveis foram invertidas para falso
-//   echo "Falso";
-
-// }
 ?>
